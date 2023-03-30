6b04a8ffb45aaf0e7ad5b72faea77b12cbac54c7
el97 <erik1337.el@gmail.com>
2023-03-30 15:07:27 +0200

    Added missing translations
    I included the Portuguese translations.

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,11 +1,15 @@
 import * as en from './languages/en.json';
 import * as nb from './languages/nb.json';
+import * as pt-BR from './languages/pt-BR.json';
+import * as pt-pt from './languages/pt-pt.json';
 import * as sv from './languages/sv.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   en: en,
   nb: nb,
+  pt-BR: pt-BR,
+  pt-pt: pt-pt,
   sv: sv,
 };
 
