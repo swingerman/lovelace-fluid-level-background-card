8f21b294b7c752dedb3174e63481bc8ec4ab2e88
Miklos Szany <miklos.szanyi@gmail.com>
2024-05-15 07:31:12 +0000

    chore: pulls card version from package

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,3 @@
-export const CARD_VERSION = '0.0.1';
 export const LEVEL_COLOR = [0, 128, 0];
 export const BACKGROUND_COLOR = [28, 28, 28];
 export const THEME_BACKGROUND_COLOR_VARIABLE = '--primary-background-color';
